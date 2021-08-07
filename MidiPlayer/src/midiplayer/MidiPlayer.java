5cccedc5a1d7c7c83020bf64ba067fced00bef17
Justin <Justin@JUSTIN-PC>
2021-08-07 00:18:53 -0400

    First commit

---
+++ MidiPlayer/src/midiplayer/MidiPlayer.java
@@ -0,0 +1,11 @@
+package midiplayer;
+
+import javax.sound.midi.MidiUnavailableException;
+
+public class MidiPlayer {
+
+	public static void main(String[] args) throws MidiUnavailableException {
+		PlayerWindow window = new PlayerWindow();
+	}
+
+}
